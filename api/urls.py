27c8d413d1d1b551d57c271b630ee6184c09d6f0
Anna <renita5701@gmail.com>
2020-04-10 23:57:56 +0600

    Test

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 # categories/<int:category_id>/ - List of all Restaurants of one Category
 # categories/<int:company_id>/<int:restaurant_id>/ - List of all Products of one Restaurant
 # categories/<int:company_id>/<int:restaurant_id>/<int:product_id> - Order of one Product
-# login/ - Login, verifying data from database
-# signup/ - Signup, add data to database
+# login/ - Login, verifying data from the database
+# signup/ - Signup, add data to the database
 
 from api.views import categories_list, category_restaurants, restaurants_products, product_order, login, signup
 
